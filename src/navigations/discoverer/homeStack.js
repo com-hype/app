91327d2d5fed1e95ab42bc5e6a6d1ab75e18131b
dantin-durand <durand.dantin.pro@gmail.com>
2022-06-18 16:13:28 +0200

    [func] add preview images

--- src/navigations/discoverer/homeStack.js
+++ src/navigations/discoverer/homeStack.js
@@ -3,10 +3,25 @@ import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import ProjectsScreen from '../../features/discoverer/projects.screen';
 import ProjectDetailsScreen from '../../features/discoverer/project-details/project-details.screen';
 import ProjectCrowfundingScreen from '../../features/discoverer/project-crowdfunding/project-crowdfunding.screen';
+import ProjectImagesScreen from '../../features/discoverer/project-images/project-images.screen';
+import {getFocusedRouteNameFromRoute} from '@react-navigation/native';
 
 const Stack = createNativeStackNavigator();
 
-const HomeStack = () => {
+const HomeStack = ({navigation, route}) => {
+  React.useLayoutEffect(() => {
+    const routeName = getFocusedRouteNameFromRoute(route);
+    if (routeName === 'ProjectImages') {
+      navigation.setOptions({
+        tabBarStyle: {display: 'none'},
+      });
+    } else {
+      navigation.setOptions({
+        tabBarStyle: {display: 'flex'},
+      });
+    }
+  }, [navigation, route]);
+
   return (
     <Stack.Navigator
       screenOptions={{
@@ -19,6 +34,7 @@ const HomeStack = () => {
         name="ProjectCrowfunding"
         component={ProjectCrowfundingScreen}
       />
+      <Stack.Screen name="ProjectImages" component={ProjectImagesScreen} />
     </Stack.Navigator>
   );
 };
